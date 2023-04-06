800871b43bb35c11a5803c90274b67803b7f3d49
Narine <115227487+narinearut@users.noreply.github.com>
2023-04-06 19:06:51 +0100

    update 01.task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', () => {
+    changeBcgColor.start()
+});
+refs.stopBtn.addEventListener('click', () => {
+    changeBcgColor.stop()
+});
+
+const changeBcgColor = {
+  intervalId: null,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.intervalId = setInterval(updateBcgColor, 1000);
+  },
+  stop() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  },
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function updateBcgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
