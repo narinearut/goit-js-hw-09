7e356dc48b6befb39685216f7f99352c11ec635f
Narine <115227487+narinearut@users.noreply.github.com>
2023-04-07 20:17:19 +0100

    hw-09.task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const input = document.querySelector('.form');
+
+input.addEventListener('submit', onInput);
+
+function onInput(e) {
+  e.preventDefault();
+  let delayEl = parseInt(e.currentTarget.elements.delay.value);
+  const stepEl = parseInt(e.currentTarget.elements.step.value);
+  const amountEl = parseInt(e.currentTarget.elements.amount.value);
+  let position = 0;
+  let numberValue = Number(delayEl);
+  let intervalId = setInterval(() => {
+    position += 1;
+    createPromise(position, numberValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    numberValue += Number(stepEl);
+    if (position === Number(amountEl)) {
+      clearInterval(intervalId);
+    }
+  }, stepEl);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
